dc6dea9a6d0638f8b50605f915e5b35f87e7f5b9
Clarice-MRS <claricerdg461@gmail.com>
2026-09-16 15:07:20 -0300

    FEAT: contador em um card

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const maisum = document.querySelector(".aumentar-quantia");
+const menosum = document.querySelector(".diminuir-quantia");
+const quantia = document.getElementById("valor-quantia");
 
+function aumentarSacola() {
+
+    let quantidade = Number(quantia.textContent);
+
+    if (quantidade < 50) {
+        quantidade++;
+    }
+
+    quantia.textContent = quantidade;
+}
+
+maisum.addEventListener("click", aumentarSacola);
+
+function diminuirSacola() {
+    let quantidade = Number(quantia.textContent);
+
+    if (quantidade > 0) {
+        quantidade--;
+    }
+
+    quantia.textContent = quantidade;
+}
+
+menosum.addEventListener("click", diminuirSacola);
